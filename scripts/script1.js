b5333ac92ecb43aa4429d0644b9189fcfeaeac4a
murzik <stas.radysh-ki2001@kep.nung.edu.ua>
2023-10-26 00:37:43 +0300

    pr2

---
+++ scripts/script1.js
@@ -0,0 +1,15 @@
+function addLeadingZero(value) {
+    return value < 10 ? "0" + value : value;
+}
+
+function updateClock() {
+    var currentTime = new Date();
+    var hours = addLeadingZero(currentTime.getHours());
+    var minutes = addLeadingZero(currentTime.getMinutes());
+    var seconds = addLeadingZero(currentTime.getSeconds());
+    var clockElement = document.getElementById('clock');
+    clockElement.textContent = hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
